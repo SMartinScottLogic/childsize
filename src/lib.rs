cbc3ed320244925a9067c83559bbd745e562e8a3
Simon Martin <havvoric@gmail.com>
2024-11-15 20:19:09 +0000

    HAV-10 Move functionality into Processor

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,28 @@
 //! Utility methods
 
-#![warn(missing_docs)]
-
+use std::cmp::Ordering;
 use std::collections::HashMap;
+use std::fmt::Display;
 use std::ops::AddAssign;
 use std::str::FromStr;
 
 use bytesize::ByteSize;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(version = "1.0", author = "Havvoric <havvoric@gmail.com>")]
+pub struct Opts {
+    pub paths: Vec<String>,
+    #[clap(short, long = "pattern")]
+    pub patterns: Vec<String>,
+
+    #[clap(short, long, value_enum)]
+    sort: SortMode,
+    #[clap(short, long)]
+    reverse: bool,
+    #[clap(short = 'z', long)]
+    show_summary: bool,
+}
 
 /// Defined orderings for results
 #[derive(Debug, Clone)]
@@ -24,7 +40,7 @@ pub enum SortMode {
 }
 
 /// Details of internal files within a folder
-#[derive(Default)]
+#[derive(Debug)]
 pub struct ChildSizeEntry {
     /// Number of internal files
     count: u64,
@@ -39,14 +55,30 @@ pub struct ChildSizeEntry {
 }
 
 impl ChildSizeEntry {
-    fn new() -> ChildSizeEntry {
-        Self {
-            min: ByteSize::pib(1),
-            ..Default::default()
-        }
-    }
     fn update_average(&mut self) {
         self.average = ByteSize::b((self.total.as_u64() as f64 / self.count as f64) as u64);
+    }
+}
+
+impl Default for ChildSizeEntry {
+    fn default() -> Self {
+        Self {
+            count: Default::default(),
+            total: Default::default(),
+            average: Default::default(),
+            max: Default::default(),
+            min: ByteSize::pib(1),
+        }
+    }
+}
+
+impl Display for ChildSizeEntry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} {} {} {}",
+            self.count, self.total, self.average, self.max
+        )
     }
 }
 
@@ -63,14 +95,6 @@ impl AddAssign<ByteSize> for ChildSizeEntry {
     }
 }
 
-static mut SUMMARY: ChildSizeEntry = ChildSizeEntry {
-    min: ByteSize::pib(1),
-    count: 0,
-    total: bytesize::ByteSize(0),
-    average: bytesize::ByteSize(0),
-    max: bytesize::ByteSize(0),
-};
-
 impl FromStr for SortMode {
     type Err = &'static str;
 
@@ -86,103 +110,124 @@ impl FromStr for SortMode {
     }
 }
 
-/// Walk a path, recording details of all immediate children
-pub fn walktree(path: &str, globset: &globset::GlobSet) -> HashMap<String, ChildSizeEntry> {
-    walkdir::WalkDir::new(path)
-        .same_file_system(true)
-        .into_iter()
-        .flatten()
-        .fold(HashMap::new(), |acc, e| filefold(acc, e, path, globset))
+type OrderProc = fn(&(&String, &ChildSizeEntry), &(&String, &ChildSizeEntry)) -> Ordering;
+
+impl SortMode {
+    fn ordering(&self) -> OrderProc {
+        match self {
+            SortMode::Count => |a, b| a.1.count.partial_cmp(&b.1.count).unwrap(),
+            SortMode::Average => |a, b| a.1.average.partial_cmp(&b.1.average).unwrap(),
+            SortMode::Total => |a, b| a.1.total.partial_cmp(&b.1.total).unwrap(),
+            SortMode::Max => |a, b| a.1.max.partial_cmp(&b.1.max).unwrap(),
+            SortMode::Min => |a, b| a.1.min.partial_cmp(&b.1.min).unwrap(),
+        }
+    }
 }
 
-/// Produce table to stdout, based on supplied sorting and direction
-pub fn process(
-    sort: SortMode,
-    reverse: bool,
-    show_summary: bool,
+#[derive(Debug)]
+pub struct Processor {
+    summary: ChildSizeEntry,
     entries: HashMap<String, ChildSizeEntry>,
-) {
-    let mut entries: Vec<_> = entries
-        .into_iter()
-        .map(|(file, mut entry)| {
-            entry.update_average();
-            (file, entry)
-        })
-        .collect();
-    match sort {
-        SortMode::Count => {
-            entries.sort_unstable_by(|a, b| a.1.count.partial_cmp(&b.1.count).unwrap())
-        }
-        SortMode::Average => {
-            entries.sort_unstable_by(|a, b| a.1.average.partial_cmp(&b.1.average).unwrap())
-        }
-        SortMode::Total => {
-            entries.sort_unstable_by(|a, b| a.1.total.partial_cmp(&b.1.total).unwrap())
-        }
-        SortMode::Max => entries.sort_unstable_by(|a, b| a.1.max.partial_cmp(&b.1.max).unwrap()),
-        SortMode::Min => entries.sort_unstable_by(|a, b| a.1.min.partial_cmp(&b.1.min).unwrap()),
-    }
-    if reverse {
-        entries.reverse();
-    }
-    for entry in entries {
-        println!(
-            "{} {} {} {} {}",
-            entry.1.count, entry.1.total, entry.1.average, entry.1.max, entry.0
-        );
-    }
-    if show_summary {
-        let summary = unsafe {
-            SUMMARY.update_average();
-            format!(
-                "{} {} {} {} {}",
-                SUMMARY.count, SUMMARY.total, SUMMARY.average, SUMMARY.max, "SUMMARY"
-            )
-        };
-        println!("{}", "=".repeat(summary.len()));
-        println!("{}", summary);
-        println!("{}", "=".repeat(summary.len()));
-    }
+    opts: Opts,
 }
 
-fn filefold(
-    mut acc: HashMap<String, ChildSizeEntry>,
-    e: walkdir::DirEntry,
-    base: &str,
-    globset: &globset::GlobSet,
-) -> HashMap<String, ChildSizeEntry> {
-    if e.file_type().is_file() && globset.is_match(e.file_name()) {
-        let key = key(e.path(), base).unwrap_or_default();
-        if let Ok(metadata) = e.metadata() {
-            //println!("key={}, path={:?}, base={}, file_name={:?}", key, e.path(), base, e.file_name());
-            let entry = acc.entry(key).or_insert_with(ChildSizeEntry::new);
-            let size = ByteSize::b(metadata.len());
-            *entry += size;
-            unsafe { SUMMARY += size };
+impl Processor {
+    pub fn new(opts: Opts) -> Self {
+        Self {
+            opts,
+            summary: ChildSizeEntry {
+                min: ByteSize::pib(1),
+                count: 0,
+                total: bytesize::ByteSize(0),
+                average: bytesize::ByteSize(0),
+                max: bytesize::ByteSize(0),
+            },
+            entries: HashMap::new(),
         }
     }
-    acc
-}
 
-fn key(path: &std::path::Path, base: &str) -> Option<String> {
-    let r = match path.strip_prefix(base) {
-        Ok(r) => r,
-        Err(_) =>
-        // should be inside base
+    /// Walk a path, recording details of all immediate children
+    fn walktree(&mut self, path: &str, globset: &globset::GlobSet) {
+        for entry in walkdir::WalkDir::new(path)
+            .same_file_system(true)
+            .into_iter()
+            .flatten()
         {
-            return None
+            self.filefold(entry, path, globset);
         }
-    };
-    match r.parent() {
-        None => Some(base.to_string()),
-        Some(p) if p == std::path::Path::new("") => Some(base.to_string()),
-        Some(parent) => Some(
-            std::path::Path::new(base)
-                .join(parent)
-                .as_os_str()
-                .to_string_lossy()
-                .to_string(),
-        ),
+        //.fold(self, |acc, e| self.filefold(acc, e, path, globset));
+    }
+
+    /// Walk all paths, recording details of encountered files
+    pub fn walktrees(&mut self) {
+        let mut builder = globset::GlobSetBuilder::new();
+        for glob in &self.opts.patterns {
+            builder.add(globset::Glob::new(glob).unwrap());
+        }
+        let globset = builder.build().unwrap();
+
+        let paths = self.opts.paths.clone();
+        for path in paths {
+            self.walktree(&path, &globset);
+        }
+    }
+
+    /// Produce table to stdout, based on supplied sorting and direction
+    pub fn process(&mut self) {
+        let mut entries: Vec<(&String, &ChildSizeEntry)> = Vec::new();
+        for (file, entry) in self.entries.iter_mut() {
+            entry.update_average();
+            entries.push((file, entry));
+        }
+        entries.sort_unstable_by(self.opts.sort.ordering());
+        if self.opts.reverse {
+            entries.reverse();
+        }
+        for entry in entries {
+            println!("{} {}", entry.1, entry.0);
+        }
+        if self.opts.show_summary {
+            self.summary.update_average();
+            let summary = format!("{} SUMMARY", self.summary);
+            println!("{}", "=".repeat(summary.len()));
+            println!("{}", summary);
+            println!("{}", "=".repeat(summary.len()));
+        }
+    }
+
+    fn filefold(&mut self, e: walkdir::DirEntry, base: &str, globset: &globset::GlobSet) {
+        if e.file_type().is_file() && globset.is_match(e.file_name()) {
+            let key = Self::key(e.path(), base).unwrap_or_default();
+            if let Ok(metadata) = e.metadata() {
+                // println!("key={}, path={:?}, base={}, file_name={:?}", key, e.path(), base, e.file_name());
+                let entry = self.entries.entry(key).or_default();
+                let size = ByteSize::b(metadata.len());
+                *entry += size;
+                self.summary += size;
+            }
+        }
+    }
+
+    fn key(path: &std::path::Path, base: &str) -> Option<String> {
+        let r = match path.strip_prefix(base) {
+            Ok(r) => r,
+            Err(_) =>
+            // should be inside base
+            {
+                return None
+            }
+        };
+        match r.parent() {
+            None => Some(base.to_string()),
+            Some(p) if p == std::path::Path::new("") => Some(base.to_string()),
+            Some(parent) => Some(
+                std::path::Path::new(base)
+                    .join(parent)
+                    .as_os_str()
+                    .to_string_lossy()
+                    .to_string(),
+            ),
+        }
     }
 }
 
@@ -196,7 +241,7 @@ mod tests {
     #[test]
     fn test_key_long() {
         let path = Path::new("/test/1/2/3/4/5.txt");
-        let r = key(path, "/test/");
+        let r = Processor::key(path, "/test/");
         assert!(r.is_some());
         assert_eq!(r.unwrap(), "/test/1");
     }
@@ -204,7 +249,7 @@ mod tests {
     #[test]
     fn test_key_normal() {
         let path = Path::new("/test/1/5.txt");
-        let r = key(path, "/test/");
+        let r = Processor::key(path, "/test/");
         assert!(r.is_some());
         assert_eq!(r.unwrap(), "/test/1");
     }
@@ -212,14 +257,14 @@ mod tests {
     #[test]
     fn test_key_no_subdir() {
         let path = Path::new("/test/5.txt");
-        let r = key(path, "/test/");
+        let r = Processor::key(path, "/test/");
         assert!(r.is_some());
         assert_eq!(r.unwrap(), "/test/");
     }
     #[test]
     fn test_key_not_rooted() {
         let path = Path::new("/test/1/5.txt");
-        let r = key(path, "/test2/");
+        let r = Processor::key(path, "/test2/");
         assert!(r.is_none());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,13 @@
-use std::collections::HashMap;
-
 use clap::Parser;
 
-use childsize::{process, walktree, ChildSizeEntry};
-
-#[derive(Parser, Debug)]
-#[clap(version = "1.0", author = "Havvoric <havvoric@gmail.com>")]
-struct Opts {
-    paths: Vec<String>,
-    #[clap(short, long, value_enum)]
-    sort: childsize::SortMode,
-    #[clap(short, long)]
-    reverse: bool,
-    #[clap(short, long = "pattern")]
-    patterns: Vec<String>,
-    #[clap(short = 'z', long)]
-    show_summary: bool,
-}
+use childsize::{Opts, Processor};
 
 fn main() {
     let opts: Opts = Opts::parse();
     println!("{opts:?}");
 
-    let mut entries: HashMap<String, ChildSizeEntry> = HashMap::new();
-
-    let mut builder = globset::GlobSetBuilder::new();
-    for glob in opts.patterns {
-        builder.add(globset::Glob::new(&glob).unwrap());
-    }
-    let globset = builder.build().unwrap();
-
-    for path in opts.paths {
-        for (k, v) in walktree(&path, &globset) {
-            entries.insert(k, v);
-        }
-    }
-    process(opts.sort, opts.reverse, opts.show_summary, entries);
+    let mut processor = Processor::new(opts);
+    processor.walktrees();
+    //println!("processor: {:?}", processor);
+    processor.process();
 }
